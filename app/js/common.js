81a8fd9c65ee4b95b12522ea21f4603f3af19d55
Dmitry Polovkov <psinegmatic@gmail.com>
2018-08-22 17:53:21 +0300

    Разметка + стили для шапки.

--- app/js/common.js
+++ app/js/common.js
@@ -1,55 +1,48 @@
-$(document).ready(function () {
-    $('.slider__wrapper-1').slick(
-        {
-            slidesToShow: 2,
-            arrows: true,
-            infinite: true,
-            nextArrow: '.btn--next-1',
-            prevArrow: '.btn--prev-1',
-        }
-    );
-
-    $('.slider-video').slick(
-        {
-            slidesToShow: 1,
-            arrows: true,
-            infinite: true,
-            draggable: false,
-            nextArrow: '.btn--next-2',
-            prevArrow: '.btn--prev-2'
-        }
-    );
-});
-
-var btnsFeedback = document.querySelectorAll('.btn__feedback');
-var modal = document.querySelector('.modal');
-var modalOverlay = document.querySelector('.modal-overlay');
-var close = document.querySelector('.modal__close');
-var body = document.querySelector('body');
-
-for (var i = 0; i < btnsFeedback.length; i++) {
-    btnsFeedback[i].addEventListener('click', function (event) {
-        event.preventDefault();
-        modal.classList.add('opened');
-        modalOverlay.classList.add('opened');
-        body.classList.add('modal-open');
-    });
+//Выпадающее мобильное меню
+function expandMenu() {
+  var burger = $('.burger-toggle');
+  burger.toggleClass('burger-toggle--close');
+  burger.siblings('.main-nav').toggleClass('main-nav--expand');
 }
 
-close.addEventListener('click', function (event) {
-   event.preventDefault();
-   modal.classList.remove('opened');
-   modalOverlay.classList.remove('opened');
-   body.classList.remove('modal-open');
+//Плавный скролл
+$(document).on('click', 'a[href^="#"]', function (event) {
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
+  expandMenu();
+  return false;
 });
 
-window.addEventListener('keydown', function(event) {
-    if (event.keyCode === 27) {
-        if (modal.classList.contains('opened') && modalOverlay.classList.contains('opened') && body.classList.contains('modal-open')) {
-            modal.classList.remove('opened');
-            modalOverlay.classList.remove('opened');
-            body.classList.remove('modal-open');
-
-        }
-    }
+//Выпадающая навигация
+$(document).ready(function () {
+  $('.burger-toggle').click(function () {
+    return expandMenu();
+  });
 });
+/*
+//Фиксированое меню
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 90) {
+    $('.main-header').addClass("main-header--fixed");
+    $('.fixed-wrapper').addClass("fixed-wrapper--fixed");
+  } else {
+    $('.main-header').removeClass("main-header--fixed");
+    $('.fixed-wrapper').removeClass("fixed-wrapper--fixed");
+  }
+});
+
+//Отображение меню при скролле вверх
+var lastScrollTop = 0;
+$(window).scroll(function (event) {
+  var st = $(this).scrollTop();
+  if (st > lastScrollTop) {
+    var burger = $('.burger-toggle');
+    burger.removeClass('burger-toggle--close');
+    burger.siblings('.main-nav').removeClass('main-nav--expand');
+    $('.fixed-wrapper').removeClass("fixed-wrapper--upsticky");
+  } else if (st > 400) {
+    $('.fixed-wrapper').addClass("fixed-wrapper--upsticky");
+  }
+  lastScrollTop = st;
+});*/
